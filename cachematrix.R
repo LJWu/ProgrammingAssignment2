d25a2d9a88c23da15f4f39fd5316681e5d9ab202
unknown <lwu1@IT056584.massey.ac.nz>
2014-05-16 16:33:43 +1200

    Clarified what each function should do

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,26 @@
-# This function is for manipulating matricies.
+# These functions attempt to optimize the manipulation of matricies.
 
-# This function inverses a matrix efficently.
+
+
+# This function creates a special matrix tha can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+return(cacheM)
+
 }
 
-# Purpose of this function is to optimize computing time when inverting a matrix
+# This function takes the output of makeCacheMatrix and computes the inverse.
+# This function also checks if the inverse has already been computed, and if so,
+# it retrieves the inverse of the matrix from the cache.
 
 cacheSolve <- function(x, ...) {
-        
+
+     if # matrix has already been inverted...
+     then
+     newmatrix <- cacheM
+     else
+     newmatrix  <-  # get the inverse here
+     
      return(newmatrix) ## Return a matrix that is the inverse of 'x'
 }
